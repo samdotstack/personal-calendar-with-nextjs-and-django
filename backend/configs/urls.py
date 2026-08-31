d3fafb48f9ee2d9e891861169b3a6f0791ab9d06
Sam Mousavi <sammousavi.dev@gmail.com>
2026-08-31 22:20:55 +0330

    freat: Add register view to account app.

--- backend/configs/urls.py
+++ backend/configs/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/planner/', include('planner.urls'))
+    path('api/planner/', include('planner.urls')),
+    path('api/auth/', include('accounts.urls'))
 ]
