00d45946d4366f2b6a3923bfbb921da6f2bc91e2
Sam Mousavi <sammousavi.dev@gmail.com>
2026-08-31 22:33:37 +0330

    feat: setup django simplejwt rest framework.

--- backend/configs/settings.py
+++ backend/configs/settings.py
@@ -107,6 +107,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 CORS_ALLOWED_ORIGINS = [
--- backend/configs/urls.py
+++ backend/configs/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/planner/', include('planner.urls')),
-    path('api/auth/', include('accounts.urls'))
+    path("admin/", admin.site.urls),
+    path("api/planner/", include("planner.urls")),
+    path("api/auth/", include("accounts.urls")),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/token/refresh/", TokenRefreshView.as_view()),
 ]
