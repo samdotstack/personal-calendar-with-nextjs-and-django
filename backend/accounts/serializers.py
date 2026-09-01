f24dc326eedf46fec0bae05279370c1984e8762b
Sam Mousavi <sammousavi.dev@gmail.com>
2026-09-01 14:41:15 +0330

    feat: Update RegisterSerializer fields to include first and last name.

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -12,7 +12,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["first_name", "last_name", "email", "password"]
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
