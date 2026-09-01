9629030be7b9c0470a1a09602ff81e9cae57d69b
Sam Mousavi <sammousavi.dev@gmail.com>
2026-09-01 14:41:04 +0330

    feat: Define User model with custom fields and UserManager integration.

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        unique=True,
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+    )
+
+    last_name = models.CharField(
+        max_length=150,
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+    )
+
+    is_staff = models.BooleanField(
+        default=False,
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
